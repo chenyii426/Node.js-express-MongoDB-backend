689d6de37a3f69d9c1b560173e07ed48f96e84cb
chenyii426 <42981394+chenyii426@users.noreply.github.com>
2018-12-11 17:16:21 -0800

    s3

---
+++ config/s3.env.js
@@ -0,0 +1,8 @@
+const env = {
+	AWS_ACCESS_KEY: 'AKIAJ3Z6VEMFCJCBULOQ',
+	AWS_SECRET_ACCESS_KEY: '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',
+	REGION : 'us-east-2',
+	Bucket: 'sci-cafe-angular'
+};
+
+module.exports = env;
